c693eb6e0ee8068cdb5d93d0e8283f42820d5d0a
AdrianaTR80 <31289704+AdrianaTR80@users.noreply.github.com>
2017-11-06 14:11:09 -0600

    Add files via upload

---
+++ calificaciones.cc
@@ -0,0 +1,43 @@
+//Adriana Tapia Ramìrez
+//06 noviembre 2017
+
+#include <iostream>
+using namespace std;
+int main ( ) {
+char cal;
+float  P, T, E, cal<=10;
+cout<<"Calificacion de examen"<<endl; 
+cin>>E;
+ cout<<"Calificacion de tareas"<<endl; 
+cin>>T;
+cout<<"Calificacion de participacion"<<endl; 
+cin>>P;
+
+switch(cal) {
+case 'A':  if(cal<8)
+else{
+cal=E*0.7+P*0.15+T*0.15;
+}
+ cout<<"Tu calificacion final es:"<<cal<<endl
+cin>>cal; break;
+
+case 'B': if (cal=>8 and tareas=0 and participacion=0)
+else {
+cal=examen;
+}
+ cout<<"Tu calificacion final es:"<<endl;
+cin>>examen;break;
+
+case 'C' : if(cal=>8 and tareas>0)
+else{
+cal=examen*0.85 + tareas;
+}
+ cout<<"Tu calificacion final es:"<<endl;
+cin>>cal; break;
+
+case 'D' : if (cal=>8 and participacion>0)
+
+return 0;
+}
+}
+
---
+++ switch.cc
@@ -0,0 +1,22 @@
+//Adriana Tapia Ramìrez
+//06 noviembre 2017
+
+#include <iostream>
+using namespace std;
+int main ( ) {
+char variable;
+cout<<"Escoge una opcion entre A,B,C,D,F"<<endl;
+cin>>variable;
+variable= toupper(variable);
+switch(variable) {
+
+case 'A' : cout<<"Excelente"<<endl; break;
+case 'B' : cout<<"Muy bien"<<endl; break;
+case 'C' : cout<<"Bien"<<endl; break;
+case 'D' : cout<<"Pasaste"<<endl; break;
+case 'F' : cout<<"Intentalo nuevamente"<<endl; break;
+default : cout<<"Opcion no valida"<<endl;
+}
+
+return 0;
+} 
